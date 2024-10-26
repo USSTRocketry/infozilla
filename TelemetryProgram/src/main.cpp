a89d39d75099b40bc30a8b335b5a7f9e488a7af9
Eric Creasy <36.creasy@gmail.com>
2024-10-26 12:53:47 -0600

    inmplemented basic terminal \n printer program

--- TelemetryProgram/src/main.cpp
+++ TelemetryProgram/src/main.cpp
@@ -1,14 +1,19 @@
 #include <Arduino.h>
 
+int i = 0;
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
+  Serial.begin(0);
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
 }
 
 void loop() {
+  i++;
+  Serial.printf("Hello World: %i\n", i);
+  delay(500);
   // put your main code here, to run repeatedly:
 }
 

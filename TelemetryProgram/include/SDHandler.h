b4314c604fb6ccd89de5b7729108c3581b9c221b
Bobby <joz548@usask.ca>
2024-12-14 15:39:25 -0600

    Initial steps for implementing SD library for Teensy

--- TelemetryProgram/include/SDHandler.h
+++ TelemetryProgram/include/SDHandler.h
@@ -1,6 +1,8 @@
 //****************************** */
 // Develop SD Handler on this project
 // When it works, push to HAL submodule
-// Use Teensi SD Port - find libraries online for implementation
+// Use Teensy SD Port - find libraries online for implementation
 // Include wrappers for options/customization when initializing SD (same as what is in HAL library already)
 //****************************** */
+
+#include <SD.h>

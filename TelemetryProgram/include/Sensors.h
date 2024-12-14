2c8c7e1fb3ce409e0e6a7c1bcb0a8f2758875e7a
Bobby <joz548@usask.ca>
2024-12-14 15:11:50 -0600

    Added HAL library and removed unnecessary cpp and header files

--- TelemetryProgram/include/Sensors.h
+++
@@ -1,6 +0,0 @@
-#include "SensorStructs.h"
-#pragma once
-
-void InitSensors();
-
-SensorData *GetSensorData();
--- TelemetryProgram/src/main.cpp
+++ TelemetryProgram/src/main.cpp
@@ -1,72 +1,81 @@
-
 #include <Arduino.h>
-#include <DataPackager.h>
-#include <DataStorage.h>
-#include <GPS.h>
-#include <Radio.h>
-#include <SDHandler.h>
-#include <Sensors.h>
-#include <SensorStructs.h>
-<<<<<<< HEAD
+// #include <DataPackager.h>
+// #include <DataStorage.h>
+// #include <GPS.h>
+// #include <Radio.h>
+// #include <SDHandler.h>
+// #include <Sensors.h>
+// #include <SensorStructs.h>
+#include <Wire.h>
+#include "Avionics_HAL.h"
 
-int i = 0;
-SensorData data;
-
-=======
-
-char data[sizeof(SensorData)];
-
-int i = 0;
-SensorData *sensorData;
-GPSData gpsData;
->>>>>>> main
-// put function declarations here:
-int myFunction(int, int);
+SensorBMP280 bmp280Sensor;
 
 void setup() {
-  Serial.begin(0);
-  // put your setup code here, to run once:
-  // int result = myFunction(2, 3);
-  
-<<<<<<< HEAD
-  InitDataPackager()
-  InitDataStorage()
-  InitRadio()
-  InitSDHandler()
-  InitSensors()
-=======
-  InitDataPackager();
-  InitDataStorage();
-  InitRadio();
-  InitSDHandler();
-  InitSensors();
->>>>>>> main
+    Serial.begin(9600);
+    if (bmp280Sensor.begin()) {
+        Serial.println("BMP280 initialized.");
+    } else {
+        Serial.println("Initialization failed.");
+    }
 }
 
 void loop() {
-  i++;
-  Serial.printf("Hello World: %i\n", i);
-  delay(500);
-  // put your main code here, to run repeatedly:
-  sensorData = GetSensorData();
-  gpsData = GetGPSData();
-
+    BMP280Data* data = bmp280Sensor.read();
+    Serial.print("Temp: ");
+    Serial.print(data->temperature);
+    Serial.print("°C, Pressure: ");
+    Serial.print(data->pressure);
+    Serial.print("Pa, Altitude: ");
+    Serial.print(data->altitude);
+    Serial.println("m");
+    delay(1000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+// char data[sizeof(SensorData)];
 
-void Handledata(){
+// int i = 0;
+// SensorData *sensorData;
+// GPSData gpsData;
+// // put function declarations here:
+// int myFunction(int, int);
 
-}
+// void setup() {
+//   Serial.begin(0);
+//   // put your setup code here, to run once:
+//   // int result = myFunction(2, 3);
+  
+//   InitDataPackager();
+//   InitDataStorage();
+//   InitRadio();
+//   InitSDHandler();
+//   InitSensors();
+// }
 
-void TransferDataToSD(){
+// void loop() {
+//   i++;
+//   Serial.printf("Hello World: %i\n", i);
+//   delay(500);
+//   // put your main code here, to run repeatedly:
+//   sensorData = GetSensorData();
+//   gpsData = GetGPSData();
 
-}
+// }
 
-char *TurnSensorDataToBytes(SensorData *data){
-  return NULL;
+// // put function definitions here:
+// int myFunction(int x, int y) {
+//   return x + y;
+// }
 
-}
+// void Handledata(){
+
+// }
+
+// void TransferDataToSD(){
+
+// }
+
+// char *TurnSensorDataToBytes(SensorData *data){
+//   return NULL;
+
+// }

32d30897a9bb0849e4000ecd554e5ba6b73b1277
Bobby <joz548@usask.ca>
2024-12-14 14:00:17 -0600

    Remove version control markers

--- TelemetryProgram/src/GPS.cpp
+++ TelemetryProgram/src/GPS.cpp
@@ -1,17 +1,9 @@
 #include <Arduino.h>
 #include <GPS.h>
 
-<<<<<<< HEAD
-gpsData data;
-
-gpsData GetGPSData() {
-    Serial.printf("GetGPSData()");
-    return data;
-=======
 GPSData gdata;
 
 GPSData GetGPSData() {
     Serial.printf("GetGPSData()");
     return gdata;
->>>>>>> main
 }
